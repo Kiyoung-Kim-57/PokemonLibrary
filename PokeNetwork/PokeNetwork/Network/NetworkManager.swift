3b05f1d575d25d11674e62228d6766c563e7e041
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-01-30 01:46:33 +0900

    feat :: 제너릭 적용한 네트워크 매니저 로직 구현

--- PokeNetwork/PokeNetwork/Network/NetworkManager.swift
+++ PokeNetwork/PokeNetwork/Network/NetworkManager.swift
@@ -9,11 +9,15 @@ import Foundation
 
 public final class NetworkManager {
     let urlSession = URLSession.shared
+    let decoder = JSONDecoder()
     
-    public func fetchData(request: HttpRequest) async throws -> Data {
+    public func fetchData<T: Decodable>(request: HttpRequest, type: T.Type) async throws -> HttpResponse<T> {
         let (data, response) = try await urlSession.data(for: request.urlRequest)
-
-        return data
+        let statusCode: Int = (response as? HTTPURLResponse)?.statusCode ?? 0
+        let dto: T = try data.toDTO(decoder: decoder)
+        let httpResponse = HttpResponse(statusCode: statusCode, dto: dto)
+        
+        return httpResponse
     }
 }
 
