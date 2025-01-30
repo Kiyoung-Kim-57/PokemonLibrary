06640f741fc99616a6090ebe5b389ed533ce6c32
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-01-30 23:58:06 +0900

    feat :: 데이터 모델 구현 및 테스트 코드 작성

---
+++ PokemonLibrary/Data/PLData/DTO/PokemonSearchResultDTO.swift
@@ -0,0 +1,12 @@
+//
+//  PokemonSearchResultDTO.swift
+//  PokemonLibrary
+//
+//  Created by 김기영 on 1/30/25.
+//
+
+
+public struct PokemonSearchResultDTO: Decodable {
+    public let name: String
+    public let url: String
+}
