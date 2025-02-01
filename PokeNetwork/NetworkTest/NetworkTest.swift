adbd7d2be1830e07354c3f273591548ac487b172
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-02-01 23:39:36 +0900

    chore :: 테스트 코드 분리

--- PokeNetwork/NetworkTest/NetworkTest.swift
+++ PokeNetwork/NetworkTest/NetworkTest.swift
@@ -16,6 +16,16 @@ final class NetworkTest: XCTestCase {
     
     override class func tearDown() { }
     
+    func test_리퀘스트_생성() throws {
+        let request = try XCTUnwrap(
+            HttpRequest(
+                scheme: .https,
+                method: .GET,
+                path: "pokeapi.co/api/v2/pokemon"
+            )
+        )
+    }
+    
     func test_포켓몬API_호출() async throws {
         let request = try XCTUnwrap(
             HttpRequest(
@@ -24,14 +34,27 @@ final class NetworkTest: XCTestCase {
                 path: "pokeapi.co/api/v2/pokemon"
             )
         )
-            .addQueryItem("offset", "10")
-            .addQueryItem("limit", "30")
         
         let data = try await networkManager.fetchData(request: request, type: PokemonListDTO.self)
         
         XCTAssertNotNil(data)
-        XCTAssertEqual(data.dto.results.count, 30, "Result: \(data.dto.results.description)")
         
     }
     
+    
+    func test_쿼리아이템_추가() async throws {
+        let request = try XCTUnwrap(
+            HttpRequest(
+                scheme: .https,
+                method: .GET,
+                path: "pokeapi.co/api/v2/pokemon"
+            )
+        )
+            .addQueryItem("offset", "10")
+            .addQueryItem("limit", "5")
+        
+        let data = try await networkManager.fetchData(request: request, type: PokemonListDTO.self)
+        
+        XCTAssertEqual(data.dto.results.count, 5, "Result: \(data.dto.results.description)")
+    }
 }
