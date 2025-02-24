556202503dbc3d8702d0927b19c66ffe70ced4ee
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-02-25 01:05:02 +0900

    feat :: UserDefaults Datasource 생성

---
+++ PokemonLibrary/Data/PLData/DataSourceImpl/PLUserDefaultsDataSource.swift
@@ -0,0 +1,30 @@
+//
+//  PLUserDefaultsDataSource.swift
+//  PokemonLibrary
+//
+//  Created by 김기영 on 2/25/25.
+//
+
+import Foundation
+
+final class PLUserDefaultsDataSource: PLReadableDataSource {
+    public typealias Item = Data
+    public typealias Condition = String
+    
+    private let standard = UserDefaults.standard
+    private let decoder = JSONDecoder()
+    private let defaultKey = ""
+    
+    func readData(requestHandler: @escaping (String) -> (String)) async throws -> Data {
+        let key = requestHandler(defaultKey)
+        guard let data = standard.object(forKey: key) as? Data else { throw DataError.notFound }
+        return Data()
+    }
+    
+    func readData<T>(type: T.Type, requestHandler: @escaping (String) -> (String)) async throws -> T where T : Decodable {
+        let key = requestHandler(defaultKey)
+        guard let data = standard.object(forKey: key) as? Data else { throw DataError.notFound }
+        let decoded = try decoder.decode(T.self, from: data)
+        return decoded
+    }
+}
