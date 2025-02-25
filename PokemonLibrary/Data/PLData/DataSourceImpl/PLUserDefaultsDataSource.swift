c66c2577c515092e2161f54028b0aae4a6436174
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-02-25 18:37:41 +0900

    feat :: PLUserDefaultsDataSource 비동기 -> 동기로 다시 수정
    - 억지로 비동기로 만드는게 오히려 성능 저하일것 같음
    - async/await 맥락만 사용할 수 있게 수정

--- PokemonLibrary/Data/PLData/DataSourceImpl/PLUserDefaultsDataSource.swift
+++ PokemonLibrary/Data/PLData/DataSourceImpl/PLUserDefaultsDataSource.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-final class PLUserDefaultsDataSource: PLDataSourceProtocol, @unchecked Sendable {
+final class PLUserDefaultsDataSource: PLDataSourceProtocol {
     public typealias Item = Data
     public typealias Condition = String
     
@@ -22,12 +22,10 @@ final class PLUserDefaultsDataSource: PLDataSourceProtocol, @unchecked Sendable 
         let key = requestHandler(defaultKey)
         
         return try await withCheckedThrowingContinuation { continuation in
-            DispatchQueue.global().async { [unowned self] in
-                if let data = standard.data(forKey: key) {
-                    continuation.resume(returning: data)
-                } else {
-                    continuation.resume(throwing: DataError.notFound)
-                }
+            if let data = standard.data(forKey: key) {
+                continuation.resume(returning: data)
+            } else {
+                continuation.resume(throwing: DataError.notFound)
             }
         }
     }
@@ -39,13 +37,11 @@ final class PLUserDefaultsDataSource: PLDataSourceProtocol, @unchecked Sendable 
         let key = requestHandler(defaultKey)
         
         return try await withCheckedThrowingContinuation { continuation in
-            DispatchQueue.global().async { [unowned self] in
-                if let data = standard.data(forKey: key),
-                   let decoded = try? decoder.decode(T.self, from: data) {
-                    continuation.resume(returning: decoded)
-                } else {
-                    continuation.resume(throwing: DataError.notFound)
-                }
+            if let data = standard.data(forKey: key),
+               let decoded = try? decoder.decode(T.self, from: data) {
+                continuation.resume(returning: decoded)
+            } else {
+                continuation.resume(throwing: DataError.notFound)
             }
         }
     }
@@ -57,10 +53,8 @@ final class PLUserDefaultsDataSource: PLDataSourceProtocol, @unchecked Sendable 
         let key = requestHandler(defaultKey)
         
         return await withCheckedContinuation { continuation in
-            DispatchQueue.global().async { [unowned self] in
-                self.standard.set(item, forKey: key)
-                continuation.resume(returning: true)
-            }
+            self.standard.set(item, forKey: key)
+            continuation.resume(returning: true)
         }
     }
     
@@ -70,15 +64,11 @@ final class PLUserDefaultsDataSource: PLDataSourceProtocol, @unchecked Sendable 
         let key = requestHandler(defaultKey)
         
         return await withCheckedContinuation { continuation in
-            DispatchQueue.global().async { [unowned self] in
-                let presentKeys = standard.dictionaryRepresentation().keys
-                
-                if presentKeys.contains(key) {
-                    standard.removeObject(forKey: key)
-                    continuation.resume(returning: true)
-                } else {
-                    continuation.resume(returning: false)
-                }
+            if standard.object(forKey: key) != nil {
+                standard.removeObject(forKey: key)
+                continuation.resume(returning: true)
+            } else {
+                continuation.resume(returning: false)
             }
         }
     }
