7a251a2c54164aabf6b8764f8786532264536352
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-02-25 18:09:14 +0900

    feat :: UserDefault를 비동기적으로 실행할 수 있게 구현

--- PokemonLibrary/Data/PLData/DataSourceImpl/PLUserDefaultsDataSource.swift
+++ PokemonLibrary/Data/PLData/DataSourceImpl/PLUserDefaultsDataSource.swift
@@ -7,29 +7,79 @@
 
 import Foundation
 
-final class PLUserDefaultsDataSource: PLReadableDataSource {
+final class PLUserDefaultsDataSource: PLDataSourceProtocol, @unchecked Sendable {
     public typealias Item = Data
     public typealias Condition = String
     
     private let standard = UserDefaults.standard
     private let decoder = JSONDecoder()
-    private let defaultKey = ""
+    private let defaultKey = "UserInfo"
     
-    func readData(requestHandler: @escaping (String) -> (String)) async throws -> Data {
+    /// ReqeustHandler는 UserDefaults의 Key를 탈출 클로저로 따로 수정할 수 있게 합니다
+    func readData(
+        requestHandler: @escaping (String) -> (String) = { return $0 }
+    ) async throws -> Data {
         let key = requestHandler(defaultKey)
-        guard let data = standard.object(forKey: key) as? Data else { throw DataError.notFound }
         
-        return data
+        return try await withCheckedThrowingContinuation { continuation in
+            DispatchQueue.global().async { [unowned self] in
+                if let data = standard.data(forKey: key) {
+                    continuation.resume(returning: data)
+                } else {
+                    continuation.resume(throwing: DataError.notFound)
+                }
+            }
+        }
     }
     
     func readData<T: Decodable>(
         type: T.Type,
-        requestHandler: @escaping (String) -> (String)
+        requestHandler: @escaping (String) -> (String) = { return $0 }
     ) async throws -> T {
         let key = requestHandler(defaultKey)
-        guard let data = standard.object(forKey: key) as? Data else { throw DataError.notFound }
-        let decoded = try decoder.decode(T.self, from: data)
         
-        return decoded
+        return try await withCheckedThrowingContinuation { continuation in
+            DispatchQueue.global().async { [unowned self] in
+                if let data = standard.data(forKey: key),
+                   let decoded = try? decoder.decode(T.self, from: data) {
+                    continuation.resume(returning: decoded)
+                } else {
+                    continuation.resume(throwing: DataError.notFound)
+                }
+            }
+        }
+    }
+    
+    func writeData(
+        _ item: Data,
+        requestHandler: @escaping (String) -> (String) = { return $0 }
+    ) async throws -> Bool {
+        let key = requestHandler(defaultKey)
+        
+        return await withCheckedContinuation { continuation in
+            DispatchQueue.global().async { [unowned self] in
+                self.standard.set(item, forKey: key)
+                continuation.resume(returning: true)
+            }
+        }
+    }
+    
+    func deleteData(
+        requestHandler: @escaping (String) -> (String) = { return $0 }
+    ) async throws -> Bool {
+        let key = requestHandler(defaultKey)
+        
+        return await withCheckedContinuation { continuation in
+            DispatchQueue.global().async { [unowned self] in
+                let presentKeys = standard.dictionaryRepresentation().keys
+                
+                if presentKeys.contains(key) {
+                    standard.removeObject(forKey: key)
+                    continuation.resume(returning: true)
+                } else {
+                    continuation.resume(returning: false)
+                }
+            }
+        }
     }
 }
