61d3af3f46a41d51d0dea3dccdedf2bb29dd6d9b
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-02-28 15:35:23 +0900

    chore :: Rename StartViewController -> HomeViewController

---
+++ PokemonLibrary/Presentation/PLPresentation/ViewController/HomeViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class HomeViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
